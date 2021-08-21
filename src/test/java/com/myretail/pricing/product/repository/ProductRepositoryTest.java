400ec7ff3e1999892686bea09dca33aeda91e4a1
Siddhartha Biswas <siddhartha100@gmail.com>
2021-08-21 11:16:07 -0500

    Mongo repository unit test added

--- src/test/java/com/myretail/pricing/product/repository/ProductRepositoryTest.java
+++ src/test/java/com/myretail/pricing/product/repository/ProductRepositoryTest.java
@@ -1,5 +1,42 @@
 package com.myretail.pricing.product.repository;
 
+import com.myretail.pricing.product.dto.CurrentPrice;
+import com.myretail.pricing.product.dto.ProductResponse;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@DataMongoTest
+@ExtendWith(SpringExtension.class)
 public class ProductRepositoryTest {
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Test
+	public void testRepository() {
+		ProductResponse dto = getMockProductResponse();
+		ProductResponse response = productRepository.save(dto);
+		assertNotNull(response);
+		assertEquals(response.get_id(),Integer.valueOf(1));
+	}
+
+	private ProductResponse getMockProductResponse() {
+		ProductResponse response = new ProductResponse();
+		CurrentPrice currentPrice = new CurrentPrice();
+		currentPrice.setCurrencyCode("USD");
+		currentPrice.setValue(10.01);
+		response.setCurrentPrice(currentPrice);
+		response.set_id(1);
+		response.setName("Computer");
+		return response;
+	}
 
 }
